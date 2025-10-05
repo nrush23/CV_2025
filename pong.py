715cbe0c5822445ccd72d7ca7081738eb1661611
N.R. <nrush@drew.edu>
2025-10-05 15:33:37 -0400

    Basic Pong set up for human visualization
    Run pong.py to see pong being run for 10s

--- pong.py
+++ pong.py
@@ -1,3 +1,19 @@
-from ale_py import ALEInterface
+import gymnasium
+import ale_py
 
-ale = ALEInterface()
+gymnasium.register_envs(ale_py)
+
+env = gymnasium.make("ALE/Pong-v5", render_mode="human")
+env.reset()
+
+#Frame rate is 60FPS, so simulation will run for 10s
+TIME = 600
+
+for _ in range(TIME):
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        obs, info = env.reset()
+
+env.close()
